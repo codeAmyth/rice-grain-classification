d69c00fd2508325dffee9eb22d44dd76c26521b6
codeAmyth <dev.amyth27@gmail.com>
2026-03-11 17:42:10 +0530

    rice classification completed

---
+++ src/predict.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+model = tf.keras.models.load_model("models/rice_classifier.h5")
+
+classes = ["Arborio","Basmati","Ipsala","Jasmine","Karacadag"]
+
+IMG_SIZE = 128
+
+
+def predict(image_path):
+
+    img = cv2.imread(image_path)
+    img = cv2.resize(img,(IMG_SIZE,IMG_SIZE))
+    img = img/255.0
+    img = np.expand_dims(img,axis=0)
+
+    prediction = model.predict(img)
+
+    index = np.argmax(prediction)
+
+    print("Rice Type:",classes[index])
+    print("Confidence:",np.max(prediction))
+
+
+predict("dataset/Rice_Image_Dataset/Basmati/basmati (14).jpg")
